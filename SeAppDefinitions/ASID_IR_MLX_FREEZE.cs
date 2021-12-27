9a8238cc8d55825b8ba1336b3eeb1f4de92cb568
RoiAtias <34520810+RoiAtias@users.noreply.github.com>
2021-12-27 09:06:03 +0200

    Create ASID_IR_MLX_FREEZE.cs

---
+++ SeAppDefinitions/ASID_IR_MLX_FREEZE.cs
@@ -0,0 +1,26 @@
+using PressSdk.CodeGenerators.Abstract.Patterns.SeDefinitions;
+using SE.ErrorsDefinitions.ErrorEvents.Enums;
+using static SE.ErrorsDefinitions.ErrorEvents.Enums.ErrorEnumerations;
+
+namespace SE.ErrorsDefinitions.ErrorEvents
+{
+    [ErrorEvent]
+    [Description("aSID temperature sensor is not responding")]
+    [Extension(nameof(ErrorEventExtension.EventHelper))]
+    [Classification(nameof(ErrorEventClassification.AsidEvents))]
+    [WhatToDo("Verify sensor wiring are not damaged")]
+    [SuspectedCause("")]
+    [EventTrigger("aSID temperature sensor is stuck")]
+    public class ASID_IR_MLX_FREEZE
+    {
+        [SensorIDParam]
+        public SensorID Sensor { get; }
+		
+		[SensorIDParam1]
+		
+        public SensorID Sensor1 { get; }
+
+    }
+}
+
+
